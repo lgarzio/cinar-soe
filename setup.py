4d31b1eb1279c64d0ed011c95d88cd18ddf3becc
Lori Garzio <lgarzio@marine.rutgers.edu>
2025-10-22 15:00:32 -0400

    update packages and setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cinar-soe',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/lgarzio/cinar-soe',
+    author='Lori Garzio',
+    author_email='lgarzio@marine.rutgers.edu'
+)
